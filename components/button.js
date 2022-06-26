569ac5564bebaa621b7b9edd3a47077310102c61
Yashvanth Babu <72075894+yvulture-7@users.noreply.github.com>
2022-06-26 11:33:11 +0530

    Added my project

--- components/button.js
+++ components/button.js
@@ -2,7 +2,7 @@ const styles = {
     button: `bg-[#6188FF] px-5 py-2 rounded-lg`,
 }
 
-const Button = ({ label, onPress }) => {
+  const Button = ({ label, onPress }) => {
     return <button className={styles.button} onClick={onPress}>{label}</button>
 }
 
